e747f715605c20ebc2491dd3c9ccad4b4003bc98
Roman Koblov <penpen938@me.com>
2014-03-16 21:08:25 +0400

    remove reading file

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from setuptools import setup
-import os.path
 import pypandoc
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='srq',
